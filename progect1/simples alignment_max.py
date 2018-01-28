91b88b64535dd19d1c3f3540abedbfa0564b7a1e
AlKoAl <32274055+AlKoAl@users.noreply.github.com>
2018-01-28 21:29:05 +0300

    Update simples alignment_max.py

--- progect1/simples alignment_max.py
+++ progect1/simples alignment_max.py
@@ -1,8 +1,7 @@
-# К величайшему сожалению, это частный случай.
 # здесь у меня уже есть две последовательности в виде строк
 # вариант с наибольшем, наименьшим:
-s1 = 'ATAT'
-s2 = 'TATA'   # НИКОГДА НЕ ЗАБЫВАТЬ, ЧТО ПЕРВОЙ КОДИРУЕСТЯ СТРОКА МАТРИЦЫ
+s1 = 'GAATTC'   # НИКОГДА НЕ ЗАБЫВАТЬ, ЧТО ПЕРВОЙ КОДИРУЕСТЯ СТРОКА МАТРИЦЫ
+s2 = 'GATTA'
 matrix = [['' for j in range(len(s1) + 2)] for i in range(len(s2) + 2)]  # i - строчки, j - стольбцы [i][j]
 k = 2
 for symbol in s1:
@@ -14,6 +13,10 @@ for symbol in s2:
     k += 1
 print(' Введите штраф за разрыв: ')
 d = int(input())
+print(' Введите штраф за совпадение: ')
+coincidence = int(input())
+print(' Введите штраф за несовпадение: ')
+discrepancy = int(input())
 for i in range(1, (len(s1) + 2)):
     matrix[1][i] = (i - 1) * d
 for i in range(1, (len(s2) + 2)):
@@ -21,9 +24,9 @@ for i in range(1, (len(s2) + 2)):
 for i in range(2, len(s1) + 2):       # Не смотреть на то как я это реализовал, а не то запутаюсь
     for j in range(2, len(s2) + 2):
         if matrix[j][0] == matrix[0][i]:
-            s = 1
+            s = coincidence
         else:
-            s = -1
+            s = discrepancy
         a = [matrix[j-1][i] + d, matrix[j][i - 1] + d, (matrix[j-1][i-1] + s)]
         a.sort()
         matrix[j][i] = a[2]
@@ -34,16 +37,16 @@ s2 = ''
 def comeback (matrix):
     global s1, s2, i, j
     if matrix[i][0] == matrix[0][j]:
-        s = 1
+        s = coincidence
     else:
-        s = -1
+        s = discrepancy
     if i == 0 and j == 0:
         s1 = s1
         s2 = s2
-    elif j >= 2 and matrix[i][j] == matrix[i][j - 1] + d:
-        s2 = matrix[0][j] + s2
-        s1 = '-' + s1
-        j -= 1
+    elif i >= 2 and matrix[i][j] == matrix[i - 1][j] + d:
+        s2 = '-' + s2
+        s1 = matrix[i][0] + s1
+        i -= 1
         comeback(matrix)
     elif i >= 2 and j >= 2 and matrix[i][j] == matrix[i - 1][j - 1] + s:
         s2 = matrix[0][j] + s2
@@ -51,10 +54,10 @@ def comeback (matrix):
         i -= 1
         j -= 1
         comeback(matrix)
-    elif i >= 2 and matrix[i][j] == matrix[i - 1][j] + d:
-        s2 = '-' + s2
-        s1 = matrix[i][0] + s1
-        i -= 1
+    elif j >= 2 and matrix[i][j] == matrix[i][j - 1] + d:
+        s2 = matrix[0][j] + s2
+        s1 = '-' + s1
+        j -= 1
         comeback(matrix)
 comeback(matrix)
 print(s2, s1)
